9896ec04ceaf42c5b66f62449892b55e9990ccd3
Alan Tsui <alantsui2022@outlook.com>
2022-12-21 19:13:07 +0800

    Add Pizza Inventory Management ASP Project

---
+++ README.md
@@ -0,0 +1,28 @@
+[ASP.NET Project Showcase](github-homepage)
+
+[![GitHub contributors][contributors]][contributors-url]
+[![GitHub stars][stars]][stars-url]
+[![GitHub forks][forks]][forks-url]
+
+__ASP.NET Project Showcase__ is a meta-repository showcasing several systems built up with __C# ASP.NET__. They are:
+
+1. Pizza Inventory Management System(RazarPagesPizza)
+
+
+# Project Showcase
+
+## 1. Pizza Inventory Management (UI + backend + Non-Presistent storage)
+__Features__
+- read pizza inventory store with following attributes
+    - Pizza Name
+    - Is Glucan Free or not {True, False}
+    - Pizza Size {Small, Medium, Large}
+    - Price {Numbers}
+- Add pizza inventory with following attributes
+    - Pizza Name
+    - Is Glucan Free or not {True, False}
+    - Pizza Size {Small, Medium, Large}
+    - Price {Numbers}
+- Delete pizza in inventory for pizza served
+- Modify pizza attribute in inventory {All attribute are editable}
+
---
+++ RazarPagesPizza/Pages/Pizza.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
+
+namespace RazarPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        [BindProperty]
+        public Pizza NewPizza { get; set; }
+
+        public List<Pizza> pizzas = new();
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            return pizza.IsGlutenFree ? "Gluten Free": "Not Gluten Free";
+        }
+
+        public void OnGet()
+        {
+            pizzas = PizzaService.GetAll();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+
+
+    }
+}
